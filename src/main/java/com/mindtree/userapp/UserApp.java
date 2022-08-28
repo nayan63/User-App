ee3810d9fad7a48463d6990ea02f05b2daa4bdd2
nayan63 <nayanhatai63@gmail.com>
2022-08-28 15:31:24 +0530

    Add Setting For Tomcat & Jasmin

--- src/main/java/com/mindtree/userapp/UserApp.java
+++ src/main/java/com/mindtree/userapp/UserApp.java
@@ -2,12 +2,19 @@ package com.mindtree.userapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UserApp {
+public class UserApp extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserApp.class, args);
 	}
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(UserApp.class);
+    }
 	
 }
